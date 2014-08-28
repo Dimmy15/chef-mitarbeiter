dd22db96088371da30b65e0e7afac5bfead6be52
Timm Letzel <timm@hojoki.com>
2014-08-28 18:51:03 +0200

    correct container name

--- recipes/mongo.rb
+++ recipes/mongo.rb
@@ -9,7 +9,8 @@
 
 docker_image 'tile/we_mongodb:v1'
 
-docker_container 'mongodb' do
+docker_container 'tile/we_mongodb:v1' do
+  contaoner_name 'mongodb'
   detach true
   port '5000:27017'
   port '5001:28017'
