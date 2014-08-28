aa9384153e86b47ae895c31a630afe51c258f38e
Timm Letzel <timm@hojoki.com>
2014-08-28 20:09:35 +0200

    Did a lot of things

--- recipes/mongo.rb
+++ recipes/mongo.rb
@@ -7,6 +7,14 @@
 # All rights reserved - Do Not Redistribute
 #
 
+directory "/opt/mongo/db" do
+  owner "root"
+  group "root"
+  mode 00644
+  recursive true
+  action :create
+end
+
 docker_image 'tile/we_mongodb:v1'
 
 docker_container 'tile/we_mongodb:v1' do
@@ -15,6 +23,7 @@ docker_container 'tile/we_mongodb:v1' do
   port '5000:27017'
   port '5001:28017'
   #env 'SETTINGS_FLAVOR=local'
-  volume '/mnt/docker:/data'
+  volume '/opt/mongo/db:/data/db'
+  cmd_timeout 600
   command '--rest --httpinterface'
 end
