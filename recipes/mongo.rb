93c898cee480372e78be4b846386f57c69d878aa
Timm Letzel <timm@hojoki.com>
2014-08-29 17:36:21 +0200

    fix or destroy port bindings; remove Container before deploying

--- recipes/mongo.rb
+++ recipes/mongo.rb
@@ -33,7 +33,7 @@ docker_image 'tile/we_mongodb:v1'
 docker_container 'tile/we_mongodb:v1' do
   container_name 'mongodb'
   detach true
-  port ['5000:27017','5001,28017']
+  port ['5000:27017','5001:28017']
   #env 'SETTINGS_FLAVOR=local'
   volume '/opt/mongo/db:/data/db'
   cmd_timeout 6000
