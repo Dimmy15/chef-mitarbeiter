e3e2a6ebeb056f4826d65b5213247c818e5ea992
Timm Letzel <timm@hojoki.com>
2014-09-03 18:28:20 +0200

    changed install of mesos

--- attributes/defaults.rb
+++ attributes/defaults.rb
@@ -1,12 +1,12 @@
-include_recipe "mesos"
-
-override[:mesos] = {
-  :type    => "mesosphere",
-  :mesosphere => {
-    :with_zookeeper => true
-  },
-  :version => "0.19.1",
-  :slave   => {
-    :isolation=> "external"
-  }
-}
+#include_recipe "mesos"
+#
+#override[:mesos] = {
+#  :type    => "mesosphere",
+#  :mesosphere => {
+#    :with_zookeeper => true
+#  },
+#  :version => "0.19.1",
+#  :slave   => {
+#    :isolation=> "external"
+#  }
+#}
